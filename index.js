641e74dbb149981b1314d6a31d355551f26eed98
Leonel Velasquez <leonelvelasquez@gmail.com>
2021-09-17 18:53:22 -0600

    Add sequelize and establish connection to localhost using postgres user and Finder DB

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 
 const express = require('express')
+const Sequelize = require('sequelize')
+
 const app = express()
 const port = 3000
 
+const sequelize = new Sequelize('postgres://postgres@localhost:5432/Finder')
+
+sequelize.authenticate().then(() => {
+  console.log('Connection has been established successfully.');
+}).catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
